8c29ae6d92cb27b847be9bf5bd7aaba91cfa5044
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-07-20 23:07:32 +0200

    Various fixes to multi
    -) fixing #24 - ordering of multi means chaining of the queued commands
    -) adds the discard keyword to multi
    -) fixes empty queue calls

--- test/redis-mock.multi.js
+++ test/redis-mock.multi.js
@@ -24,6 +24,15 @@ describe("multi()", function () {
   });
 
   describe("exec()", function () {
+    it("should handle empty queues", function (done) {
+      var multi = r.multi();
+      multi.exec(function (err, results) {
+        should(err).not.be.ok();
+        should.deepEqual(results, []);
+        done();
+      });
+    });
+
     it("should handle things without errors and callbacks", function (done) {
       var multi = r.multi();
       multi.get('foo').incr('foo');
@@ -58,5 +67,44 @@ describe("multi()", function () {
       }).exec();
     });
 
+    it("should run sorted set operations in order", function (done) {
+      r.zadd('myzset', 1, 'a');
+      r.zadd('myzset', 2, 'b');
+      r.multi().
+        zremrangebyscore('myzset', 0, 1).
+        zadd('myzset', 'NX', 3, 'a').
+        exec(function (err, result) {
+          should(err).not.be.ok();
+          should.deepEqual(result, [1, 1]);
+          done();
+        });
+    });
+  });
+
+  describe("discard()", function () {
+    it("should properly discard the command queue", function (done) {
+      r.set('foo', 3, function() {
+        var multi = r.multi()
+        multi.incr('foo', function () {
+          // Discarded queues aren't calling their callbacks.
+          should.not.be.ok(true);
+        })
+        multi.discard();
+        r.get('foo', function (err, value) {
+          value.should.eql('3')
+          done();
+        });
+      });
+    });
+
+    it("should now allow to re-run the command queue", function (done) {
+      var multi = r.multi()
+      multi.discard()
+      multi.exec(function (err, value) {
+        should(value).not.be.ok();
+        err.should.be.ok();
+        done();
+      });
+    });
   });
 });
