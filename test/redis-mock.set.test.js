442ca9dd7ac464c6c9207d602ca1befc91f71be6
Paul <cronald@gmail.com>
2017-07-23 13:30:43 +0100

    Add SMOVE support (#34)
    * Add support for SMOVE command
    
    * Remove whitespace

--- test/redis-mock.set.test.js
+++ test/redis-mock.set.test.js
@@ -244,3 +244,168 @@ describe('scard', function () {
   });
 
 });
+
+describe('smove', function () {
+
+  it('should remove the element from the source if it exists', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', 'qux', function (err, result) {
+
+      r.smove('foo', 'bar', 'baz', function (err, result) {
+        result.should.eql(1);
+
+        r.smembers('foo', function (err, result) {
+          result.should.be.instanceof(Array);
+          result.should.have.length(1);
+          result.should.containEql('qux');
+
+          r.end(true);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should add the element to the destination if it exists', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', 'qux', function (err, result) {
+      r.sadd('bar', 'qux', function (err, result) {
+
+        r.smove('foo', 'bar', 'baz', function (err, result) {
+          result.should.eql(1);
+
+          r.smembers('bar', function (err, result) {
+            result.should.be.instanceof(Array);
+            result.should.have.length(2);
+            result.should.containEql('qux');
+            result.should.containEql('baz');
+
+            r.end(true);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('should add the element to the destination if it does not exist', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', 'qux', function (err, result) {
+
+      r.smove('foo', 'bar', 'baz', function (err, result) {
+        result.should.eql(1);
+
+        r.smembers('bar', function (err, result) {
+          result.should.be.instanceof(Array);
+          result.should.have.length(1);
+          result.should.containEql('baz');
+
+          r.end(true);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should do nothing if the element is not a member of source', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', function (err, result) {
+      r.sadd('bar', 'quux', function(err, result) {
+
+        r.smove('foo', 'baz', 'qux', function (err, result) {
+          result.should.eql(0);
+
+          r.smembers('bar', function (err, result) {
+            result.should.be.instanceof(Array);
+            result.should.have.length(1);
+            result.should.containEql('quux');
+
+            r.smembers('foo', function(err, result) {
+              result.should.be.instanceof(Array);
+              result.should.have.length(1);
+              result.should.containEql('baz');
+              r.end(true);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it('should return error when the value stored at the source key is not a set', function (done) {
+    var r = redismock.createClient();
+    r.hset('foo', 'baz', 'qux', function (err, result) {
+
+      r.smove('foo', 'bar', 'baz', function (err, result) {
+        err.message.should.eql('WRONGTYPE Operation against a key holding the wrong kind of value');
+
+        r.end(true);
+        done();
+      });
+    });
+  });
+
+  it('should return error when the value stored at the destination key is not a set', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', function (err, result) {
+      r.hset('bar', 'baz', 'qux', function (err, result) {
+
+        r.smove('foo', 'bar', 'baz', function (err, result) {
+          err.message.should.eql('WRONGTYPE Operation against a key holding the wrong kind of value');
+
+          r.end(true);
+          done();
+        });
+
+      });
+    });
+  });
+
+  it('should ignore members that are already a member of the set', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', function (err, result) {
+      r.sadd('bar', 'baz', function (err, result) {
+
+        r.smove('foo', 'bar', 'baz', function(err, result) {
+          result.should.eql(1);
+
+          r.smembers('foo', function (err, result) {
+            result.should.be.instanceof(Array);
+            result.should.have.length(0);
+
+            r.smembers('bar', function (err, result) {
+              result.should.be.instanceof(Array);
+              result.should.have.length(1);
+              result.should.containEql('baz');
+
+              r.end(true);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it('should support arguments without callback', function (done) {
+    var r = redismock.createClient();
+    r.sadd('foo', 'baz', function(err, result) {
+
+      r.smove('foo', 'bar', 'baz');
+      r.smembers('foo', function (err, result) {
+        result.should.be.instanceof(Array);
+        result.should.have.length(0);
+
+        r.smembers('bar', function (err, result) {
+          result.should.be.instanceof(Array);
+          result.should.have.length(1);
+          result.should.containEql('baz');
+
+          r.end(true);
+          done();
+        });
+      });
+    });
+  });
+});
