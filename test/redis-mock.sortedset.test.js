b4c174be654d5bd05eb4d9c1bfbcb12e3bfe6150
Dawid Loubser <dawid.loubser@ibi.co.za>
2018-07-02 17:09:47 +0200

    Initial support for ZUNIONSTORE (incomplete, no WEIGHTS and AGGREGATE)

--- test/redis-mock.sortedset.test.js
+++ test/redis-mock.sortedset.test.js
@@ -644,6 +644,97 @@ describe("zscore", function () {
   });
 });
 
+describe("zunionstore", function () {
+  var nonExistentKey = '$#$#@#%%';
+  var nonExistentMember = '@#@##$';
+  var testKey1 = "zscoreKey1";
+  var testScore1 = 100.00;
+  var testMember1 = JSON.stringify({'a': 'b'});
+
+  it("performs set union across three existing sets", function (done) {
+    r.zadd("zsrc1", 1.1, "mem:1:1", 2.1, "mem:1:2", 3.1, "mem:1:3", function(err, result) {
+      r.zadd("zsrc2", 1.2, "mem:2:1", 2.2, "mem:2:2", function(err, result) {
+        r.zadd("zsrc3", 1.3, "mem:3:1", 2.3, "mem:3:2", 3.3, "mem:3:3", function(err, result) {
+          // Store the union of all three
+          r.zunionstore("zdest1", 3, "zsrc1", "zsrc2", "zsrc3", function(error, result) {
+            // Pull members and inspect
+            result.should.equal(8)
+            r.zrange(['zdest1', '0', '-1', 'withscores'], function(err, result) {
+              result.should.deepEqual([ 
+              'mem:1:1',
+              '1.1',
+              'mem:2:1',
+              '1.2',
+              'mem:3:1',
+              '1.3',
+              'mem:1:2',
+              '2.1',
+              'mem:2:2',
+              '2.2',
+              'mem:3:2',
+              '2.3',
+              'mem:1:3',
+              '3.1',
+              'mem:3:3',
+              '3.3' ])
+              done();
+            });
+          })
+        });
+      });
+    });
+  });
+
+  it("supports non-existent sets", function (done) {
+    r.zadd("zsrc1", 1.1, "mem:1:1", 2.1, "mem:1:2", 3.1, "mem:1:3", function(err, result) {
+      r.zadd("zsrc2", 1.2, "mem:2:1", 2.2, "mem:2:2", function(err, result) {
+        // Store the union of all three (but third one does not exist)
+        r.zunionstore("zdest1", 3, "zsrc1", "zsrc2", "zsrc3", function(error, result) {
+          // Pull members and inspect
+          result.should.equal(5)
+          r.zrange(['zdest1', '0', '-1', 'withscores'], function(err, result) {
+            result.should.deepEqual([ 
+            'mem:1:1',
+            '1.1',
+            'mem:2:1',
+            '1.2',
+            'mem:1:2',
+            '2.1',
+            'mem:2:2',
+            '2.2',
+            'mem:1:3',
+            '3.1'])
+            done();
+          });
+        })
+      });
+    });
+  });
+
+  it("use to copy a single set", function (done) {
+    r.zadd("zsrc1", 1.1, "mem:1:1", 2.1, "mem:1:2", 3.1, "mem:1:3", function(err, result) {
+      // Store the union of all three (but third one does not exist)
+      r.zunionstore("zdest1", 1, "zsrc1", function(error, result) {
+        // Pull members and inspect
+        result.should.equal(3)
+        r.zrange(['zdest1', '0', '-1', 'withscores'], function(err, result) {
+          result.should.deepEqual([ 
+          'mem:1:1',
+          '1.1',
+          'mem:1:2',
+          '2.1',
+          'mem:1:3',
+          '3.1'])
+          done();
+        });
+      })
+    });
+  });
+
+  // TODO: [WEIGHTS weight [weight ...]] 
+  // TODO: [AGGREGATE SUM|MIN|MAX]
+});
+
 describe("sortedset multi commands", function () {
   var testKey1 = "sortedsetmultiKey1";
 
