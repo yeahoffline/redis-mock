3d50314cb3305901f357535ac98961b48871a87b
millanp <myGitGit2013>
2019-08-23 14:25:36 -0700

    add test to check error handling of invalid db index

--- test/redis-mock.server.test.js
+++ test/redis-mock.server.test.js
@@ -12,6 +12,45 @@ afterEach(function (done) {
   r.quit(done);
 });
 
+describe("auth", function () {
+  it("should always succeed and call back", function (done) {
+    r.auth("secret", function (err, result) {
+      result.should.equal('OK');
+      done();
+    });
+  });
+});
+
+describe("select", function () {
+  it("should change the currently selected database", function (done) {
+    r.select(0, function (err, result) {
+      result.should.be.equal('OK');
+      r.set("a", "1", function (err, result) {
+        r.select(1, function (err, result) {
+          result.should.be.equal('OK');
+          r.exists("a", function (err, result) {
+            result.should.be.equal(0);
+            r.select(0, function (err, result) {
+              result.should.be.equal('OK');
+              r.get("a", function (err, result) {
+                result.should.be.equal("1");
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  it("should throw an error when given an invalid database index", function (done) {
+    r.select(17, function (err, result) {
+      should.exist(err);
+      done();
+    });
+  });
+});
+
 describe("flushdb", function () {
 
   it("should clean the current database", function (done) {
@@ -33,46 +72,16 @@ describe("flushdb", function () {
 
   });
 
-  it("should leave other databases intact", function(done) {
-    r.select(0, function(err, result) {
-      r.set("a", "1", function(err, result) {    
-        r.select(3, function(err, result) {
-          r.flushdb(function(err, result) {
-            r.select(0, function(err, result) {
-              r.get("a", function(err, result) {
+  it("should leave other databases intact", function (done) {
+    r.select(0, function (err, result) {
+      r.set("a", "1", function (err, result) {
+        r.select(3, function (err, result) {
+          r.flushdb(function (err, result) {
+            r.select(0, function (err, result) {
+              r.get("a", function (err, result) {
                 result.should.be.equal("1");
                 done();
               })
-            });
-          });
-        });
-      });
-    });
-  });
-});
-
-describe("auth", function () {
-  it("should always succeed and call back", function (done) {
-    r.auth("secret", function (err, result) {
-      result.should.equal('OK');
-      done();
-    });
-  });
-});
-
-describe("select", function() {
-  it("should change the currently selected database", function (done) {
-    r.select(0, function(err, result) {
-      result.should.be.equal('OK');
-      r.set("a", "1", function(err, result) {
-        r.select(1, function(err, result) {
-          r.exists("a", function(err, result) {
-            result.should.be.equal(0);
-            r.select(0, function (err, result) {
-              r.get("a", function(err, result) {
-                result.should.be.equal("1");
-                done();
-              });
             });
           });
         });
